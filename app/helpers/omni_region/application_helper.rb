3d9f1ed6b5fa2a08953869e5344e22209d01a0be
Andersen Fan <as181920@gmail.com>
2019-02-14 11:21:39 +0800

    Add helper for province/city/district selection

--- app/helpers/omni_region/application_helper.rb
+++ app/helpers/omni_region/application_helper.rb
@@ -1,4 +1,15 @@
 module OmniRegion
   module ApplicationHelper
+    def province_options_for_select(country_code, selected_code)
+      options_for_select(OmniRegion::Province.where(parent_id: OmniRegion::Country.find_by(code: country_code || 156)&.id).pluck(:name, :code), selected_code)
+    end
+
+    def city_options_for_select(province_code, selected_code)
+      options_for_select(OmniRegion::City.where(parent_id: OmniRegion::Province.find_by(code: province_code)&.id).pluck(:name, :code), selected_code)
+    end
+
+    def district_options_for_select(city_code, selected_code)
+      options_for_select(OmniRegion::District.where(parent_id: OmniRegion::City.find_by(code: city_code)&.id).pluck(:name, :code), selected_code)
+    end
   end
 end
