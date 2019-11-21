b2cae4295a219431c2412bff1f69462b7d217f71
Andersen Fan <as181920@gmail.com>
2019-11-21 11:34:14 +0800

    Fix typo

--- lib/omni_region.rb
+++ lib/omni_region.rb
@@ -6,8 +6,8 @@ module OmniRegion
   included do
     # belongs_to :division, class_name: "::OmniRegion::Division", foreign_key: :division_code, primary_key: :code, optional: true
     # belongs_to :country, class_name: "::OmniRegion::Country", foreign_key: :country_code, primary_key: :code, optional: true
-    # belongs_to :province, class_name: "::OmniRegion::Country", foreign_key: :province_code, primary_key: :code, optional: true
-    # belongs_to :city, class_name: "::OmniRegion::Country", foreign_key: :city_code, primary_key: :code, optional: true
-    # belongs_to :district, class_name: "::OmniRegion::Country", foreign_key: :district_code, primary_key: :code, optional: true
+    # belongs_to :province, class_name: "::OmniRegion::Province", foreign_key: :province_code, primary_key: :code, optional: true
+    # belongs_to :city, class_name: "::OmniRegion::City", foreign_key: :city_code, primary_key: :code, optional: true
+    # belongs_to :district, class_name: "::OmniRegion::District", foreign_key: :district_code, primary_key: :code, optional: true
   end
 end
